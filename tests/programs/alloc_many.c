a338a60677874fc96042ac63d422221eb4de45e0
victoryang00 <yangyiwei2000@gmail.com>
2022-10-29 15:29:58 -0700

    add riscv

--- tests/programs/alloc_many.c
+++ tests/programs/alloc_many.c
@@ -1,9 +1,19 @@
+#include <stdlib.h>
+int init_random(int *a, int *b) {
+  for (int i = 0; i < 256 * 1024; i++) {
+    a[i] = rand();
+    b[i] = rand();
+  }
+}
 int main() {
-    int a[256*1024] = {};
-    for (int i = 0; i < 256*1024; i++) {
-        if (a[i] != 0) {
-            return 1;
-        }
-    }
-    return 0;
+  int a[256 * 1024] = {};
+  int b[256 * 1024] = {};
+
+  for (int i = 0; i < 256 * 1024; i++) {
+    if (rand() > 100)
+      a[i] = a[i + 1] * rand() + b[i];
+    else
+      a[i] = a[i - 1] * rand() + b[i];
+  }
+  return 0;
 }
